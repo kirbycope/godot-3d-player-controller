896bad71dca9c6989680cd648d560d97f1da5237
Timothy Cope <kirbycope@gmail.com>
2025-01-11 12:37:25 -0800

    top_level rm

--- addons/3d_player_controller/virtual_controller_3d.gd
+++ addons/3d_player_controller/virtual_controller_3d.gd
@@ -232,10 +232,3 @@ func _input(event: InputEvent) -> void:
 
 	# Redraw canvas items via `_draw()`
 	queue_redraw()
-
-
-## Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-
-	# This CanvasItem will not inherit its transform from parent CanvasItems. 
-	top_level = true
