976cbd3e52c33bafcb241ee10fe0848703455996
Timothy Cope <kirbycope@gmail.com>
2025-02-11 10:50:27 -0800

    quack quack

--- scenes/duck.gd
+++ scenes/duck.gd
@@ -4,22 +4,31 @@ var player: CharacterBody3D
 var follow_distance: float = 1.0
 var follow_speed: float = 2.25
 var gravity: float = ProjectSettings.get("physics/3d/default_gravity")
+const quack_1 = preload("res://assets/duck/quack_1.wav")
+const quack_2 = preload("res://assets/duck/quack_2.wav")
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
+
 	# Check if the body is a Player
 	if body.is_in_group("Player"):
 		player = body
+		audio_player.stream = quack_2
+		audio_player.play()
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
+
 	# Check if the body is a Player
 	if body.is_in_group("Player"):
 		player = null
 		velocity.x = 0
 		velocity.z = 0
+		audio_player.stream = quack_1
+		audio_player.play()
 
 
 func _process(delta: float) -> void:
